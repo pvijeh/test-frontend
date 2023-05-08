7c708c9cf1cd49b157cddd70fad0d73a914eba8a
Peter Vijeh <pvijeh@gmail.com>
2023-05-07 22:10:44 -0400

    fix

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,14 +1,16 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
 import App from "./App"
 
 test("renders learn react link", () => {
-  const { getByText } = render(
+  render(
     <Provider store={store}>
       <App />
     </Provider>,
   )
 
-  expect(getByText(/learn/i)).toBeInTheDocument()
+  const element = screen.getByText("this is a test")
+
+  expect(element).toBeInTheDocument()
 })
